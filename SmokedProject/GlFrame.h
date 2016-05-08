2d1bafcdb5122c0e119428ba82397aaf7a4082a7
sexysoushi <barbara1603@hotmail.com>
2016-05-08 12:16:15 +0200

    Clean

---
+++ SmokedProject/GlFrame.h
@@ -0,0 +1,28 @@
+﻿#ifndef GlFrame_h
+#define GlFrame_h
+
+
+#include "GlWindow.h"
+
+
+class GlFrame : public GlWindow
+{
+	public:
+        GlFrame();
+        ~GlFrame();
+
+		/*!
+		*\brief Initialization des objets.
+		*/
+		bool initializeObjects();
+
+		/*!
+		*\brief Rendu de la scène.
+		*/
+		void render();
+
+		void keyPressEvent(QKeyEvent *);
+};
+
+
+#endif
--- SmokedProject/main.cpp
+++ SmokedProject/main.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include "TP01.h"
+#include "GlFrame.h"
 
 int main(int argc, char *argv[])
 {
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 //    MainWindow w;
 //    w.show();
 
-    TP01 win;
+    GlFrame win;
     win.show();
 
     return a.exec();
